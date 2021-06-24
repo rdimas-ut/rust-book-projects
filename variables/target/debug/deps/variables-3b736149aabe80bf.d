11aceffa5c835501a93c1f279d38b9107d0c9239
Ruben Dimas <57540512+rdimas-ut@users.noreply.github.com>
2021-06-23 23:29:50 -0500

    variables

---
+++ variables/target/debug/deps/variables-3b736149aabe80bf.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/Ruben Dimas/Desktop/Rust Book Projects/variables/target/debug/deps/variables-3b736149aabe80bf: src/main.rs
+
+/mnt/c/Users/Ruben Dimas/Desktop/Rust Book Projects/variables/target/debug/deps/variables-3b736149aabe80bf.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/Ruben\ Dimas/Desktop/Rust\ Book\ Projects/variables/target/debug/variables: /mnt/c/Users/Ruben\ Dimas/Desktop/Rust\ Book\ Projects/variables/src/main.rs
